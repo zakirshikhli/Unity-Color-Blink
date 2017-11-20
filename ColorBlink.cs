9624ac804b4290e34c4fa9fe5a1c997c667ca174
Zakir Shikhli <zakirshikhli@gmail.com>
2017-11-20 20:05:51 +0400

    Update ColorBlink.cs

--- ColorBlink.cs
+++ ColorBlink.cs
@@ -9,24 +9,28 @@ public class ColorBlink : MonoBehaviour {
     Color32 tempColor = new Color32(255, 20, 20, 0);
     bool fadeBool = false;
 
-
+void MyFunction(){
     
-    IEnumerator CellFade() {
-        if (fadeBool) {
-            fadeBool = false;
+if (something happened) {
+    
+    fadeBool = true;
+    
+}
+    
+    IEnumerator CellFade() {        
+        fadeBool = false;
 
-            for (int i = 0; i < 20; i++) {
-                square.color = tempColor;
-                tempColor = new Color32(255, 20, 20, tempColor.a += 10);
-                yield return new WaitForSeconds(0.03f);
-            }
-
-            for (int i = 0; i < 19; i++) {
-                square.color = tempColor;
-                tempColor = new Color32(255, 20, 20, tempColor.a -= 10);
-                yield return new WaitForSeconds(0.03f);
-            }
+        for (int i = 0; i < 20; i++) {
+            square.color = tempColor;
+            tempColor = new Color32(255, 20, 20, tempColor.a += 10);
+            yield return new WaitForSeconds(0.03f);
         }
+
+        for (int i = 0; i < 19; i++) {
+            square.color = tempColor;
+            tempColor = new Color32(255, 20, 20, tempColor.a -= 10);
+            yield return new WaitForSeconds(0.03f);
+        }        
     }    
     
 
