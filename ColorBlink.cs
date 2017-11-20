f4366552c2e019471f8623f7e8253cd48be37957
Zakir Shikhli <zakirshikhli@gmail.com>
2017-11-20 19:55:19 +0400

    Update ColorBlink.cs

--- ColorBlink.cs
+++ ColorBlink.cs
@@ -1,41 +1,42 @@
 using System.Collections;
 using UnityEngine;
 
-public class ColorBlink : MonoBehaviour
-{   
+
+public class ColorBlink : MonoBehaviour {  
+    
+    
     public SpriteRenderer square;
     Color32 tempColor = new Color32(255, 20, 20, 0);
     bool fadeBool = false;
 
 
-    IEnumerator CellFade()
-    {
-        if (fadeBool)
-        {
+    
+    IEnumerator CellFade() {
+        if (fadeBool) {
             fadeBool = false;
 
-            for (int i = 0; i < 20; i++)
-            {
+            for (int i = 0; i < 20; i++) {
                 square.color = tempColor;
                 tempColor = new Color32(255, 20, 20, tempColor.a += 10);
                 yield return new WaitForSeconds(0.03f);
             }
 
-            for (int i = 0; i < 19; i++)
-            {
+            for (int i = 0; i < 19; i++) {
                 square.color = tempColor;
                 tempColor = new Color32(255, 20, 20, tempColor.a -= 10);
                 yield return new WaitForSeconds(0.03f);
             }
         }
-
     }    
+    
 
-    private void Update()
-    {
-        if (fadeBool)
-        {
+    void Update() {
+        
+        if (fadeBool) {
             StartCoroutine(CellFade());
         }
+        
     }
+    
+    
 }
