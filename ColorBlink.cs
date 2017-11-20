2feb135573a5eee1d93cf955a6b68ec0af322521
Zakir Shikhli <zakirshikhli@gmail.com>
2017-11-20 20:31:47 +0400

    Update ColorBlink.cs

--- ColorBlink.cs
+++ ColorBlink.cs
@@ -8,7 +8,7 @@ public class ColorBlink : MonoBehaviour {
     public SpriteRenderer square; //object to blink
     Color32 tempColor = new Color32(255, 20, 20, 0); 
     bool blinkBool = false;
-
+    float blinkSpeed = 0.03f // less is faster
     
 void MyFunction() {
     
@@ -21,34 +21,29 @@ void MyFunction() {
     
     IEnumerator BlinkNow() { 
         
-        blinkBool = false;
-        Color32 firstColor = square.color;
-        
+        blinkBool = false; // prevent loop more than once
+        Color32 firstColor = square.color; // to restore first color
+        // to red
         for (int i = 0; i < 20; i++) {
             square.color = tempColor;
             tempColor = new Color32(255, 20, 20, tempColor.a += 10);
-            yield return new WaitForSeconds(0.03f);
+            yield return new WaitForSeconds(blinkSpeed);
         }
-
-        for (int i = 0; i < 19; i++) {
+        // back to transparence
+        for (int i = 0; i < 19; i++) { // not 20 to prevent exception chance
             square.color = tempColor;
             tempColor = new Color32(255, 20, 20, tempColor.a -= 10);
-            yield return new WaitForSeconds(0.03f);
-        }   
-        
+            yield return new WaitForSeconds(blinkSpeed);
+        }  
+        //restore first color
         square.color = firstColor;
-
-    }    
-    
+    }   
 
     
-    void Update() {
-        
+    void Update() {        
         if (blinkBool) {
             StartCoroutine(BlinkNow());
-        }
-        
-    }
-    
+        }        
+    }    
     
 }
